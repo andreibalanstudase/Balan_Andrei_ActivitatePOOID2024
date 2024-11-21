466b8e047994f1dc5807f7ee16cb1d1d53ed2504
balanandreistudase <balanandrei23@stud.ase.ro>
2024-11-22 00:54:41 +0200

    Tema1

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -1,6 +1,132 @@
+#define CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
-void main() {
-	cout << "Hello!";
+class Autobuz {
+private:
+    static int nrAutobuze;
+    const int idAutobuz;
+    int capacitate;
+    int nrPersoaneImbarcate;
+    char* producator;
+public:
+
+    // Constructorul fara parametrii
+    Autobuz() : idAutobuz(++nrAutobuze) {
+        this->capacitate = 30;
+        this->nrPersoaneImbarcate = 0;
+        this->producator = new char[strlen("Default") + 1];
+        strcpy(producator, "Default");
+    }
+
+    // Constructorul cu parametrii
+    Autobuz(int capacitate, int nrPersoaneImbarcate, const char* producator) : idAutobuz(++nrAutobuze) {
+        this->capacitate = capacitate;
+        this->nrPersoaneImbarcate = nrPersoaneImbarcate;
+        this->producator = new char[strlen(producator) + 1];
+        strcpy(this->producator, producator);
+    }
+
+    // Constructorul de copiere
+    Autobuz(const Autobuz& a) : idAutobuz(++nrAutobuze) {
+        this->capacitate = a.capacitate;
+        this->nrPersoaneImbarcate = a.nrPersoaneImbarcate;
+        this->producator = new char[strlen(a.producator) + 1];
+        strcpy(this->producator, a.producator);
+    }
+
+    // Supraincarcarea operatorului =
+    Autobuz operator=(const Autobuz& a) {
+        if (this->producator != NULL)
+        {
+            delete[]this->producator;
+
+            this->capacitate = a.capacitate;
+            this->nrPersoaneImbarcate = nrPersoaneImbarcate;
+            this->producator = new char[strlen(a.producator) + 1];
+            strcpy(producator, a.producator);
+        }
+        return *this;
+    }
+
+    // Supraincarcarea opeartorului de cast explicit int
+    explicit operator int() {
+        return this->capacitate - this->nrPersoaneImbarcate;
+    }
+
+    // Getter Id Autobuz
+    int getIdAutobuz() {
+        return this->idAutobuz;
+    }
+
+    // Getter capacitate
+    int getCapacitate() {
+        return this->capacitate;
+    }
+
+    // Setter capacitate
+    void setCapacitate(int capacitate) {
+        this->capacitate = capacitate;
+    }
+
+    // Getter numar persoane imbarcate
+    int getNrPersoaneImbarcate() {
+        return this->nrPersoaneImbarcate;
+    }
+
+    // Setter numar persoane imbarcate
+    void setNrPersoaneImbarcate(int nrPersoaneImbarcate) {
+        this->nrPersoaneImbarcate = nrPersoaneImbarcate;
+    }
+
+    // Getter locuri libere (diferenta intre capacitate si numarul de persoane imbarcate)
+    int getLocuriLibere() {
+        return (this->capacitate - this->nrPersoaneImbarcate);
+    }
+
+    // Destructor in care se sterge pointerul pentru a evita memory leaks
+    ~Autobuz() {
+        cout << endl << "S-a apelat destructorul!";
+        delete[] producator;
+    }
+
+    // Utilizarea cuvantului cheie friend pentru a supraincarca operatorul <<
+    friend ostream& operator<<(ostream& oStream, Autobuz& autobuz);
+
+};
+
+// Supraincarcarea operatorului <<
+ostream& operator<< (ostream& oStream, Autobuz& autobuz) {
+    oStream << autobuz.getIdAutobuz() << "; " << autobuz.getCapacitate() << "; " << autobuz.getNrPersoaneImbarcate();
+    return oStream;
+}
+
+// Supraincarcarea operatorului > pentru compararea disponibilitatii in autobuze
+bool operator>(Autobuz& a, Autobuz& b) {
+    if (a.getLocuriLibere() > b.getLocuriLibere())
+    {
+        cout << "Primul autobuz are mai multe locuri libere" << endl;
+        return true;
+    }
+    else
+    {
+        cout << "Al doilea autobuz are mai multe locuri libere" << endl;
+        return false;
+    }
+}
+
+// Initializarea numarului de autobuze cu 0
+int Autobuz::nrAutobuze = 0;
+
+int main() {
+    Autobuz autobuz1;
+    cout << autobuz1.getCapacitate() << endl;
+    cout << autobuz1.getNrPersoaneImbarcate() << endl;
+    Autobuz autobuz2(40, 20, "Mercedes");
+    Autobuz autobuz3 = autobuz2;
+    autobuz3.getNrPersoaneImbarcate();
+    autobuz3 > autobuz1;
+    return 0;
 }
